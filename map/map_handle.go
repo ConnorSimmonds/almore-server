d4ca56b085fedfc7cc161513ebe188156f1ac9a8
Connor Simmonds <connor.t.simmonds@gmail.com>
2020-02-17 22:53:50 +1300

    The map_handle.go file now has two functions - they are currently empty, but will be properly filled out at a later date.

--- map/map_handle.go
+++ map/map_handle.go
@@ -1,1 +1,13 @@
+//The map package handles all of the dungeon map file handling. Through this, we're able to update the map files,
+//and so on.
 package _map
+
+//Updates the map at the position of arguments x, y with the value of argument value
+func UpdateMap(x int, y int, value int) {
+
+}
+
+//Creates the map, if it's not found already.
+func createMap() {
+
+}
