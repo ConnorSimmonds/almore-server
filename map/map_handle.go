ab6c27eb10771ea0ecd863ff330296dd98830fbb
ConnorSimmonds <connor.t.simmonds@gmail.com>
2020-02-18 21:21:39 +1300

    Actually committing the map_handle.go and init_user.go files from the previous commit.

--- map/map_handle.go
+++ map/map_handle.go
@@ -2,12 +2,19 @@
 //and so on.
 package _map
 
+import (
+	"os"
+)
+
 //Updates the map at the position of arguments x, y with the value of argument value
 func UpdateMap(x int, y int, value int) {
 
 }
 
-//Creates the map, if it's not found already.
+//Creates the map
 func createMap() {
+	fileLocation := "Maps/"
+	//We need to get the user ID etc. in memory
 
+	os.Create(fileLocation)
 }
--- user/init_user.go
+++ user/init_user.go
@@ -1,10 +1,14 @@
 //This package handles all of the user modification (i.e. initializing on log-in).
 package user
 
-//This initializes a user upon log-in. This
-//TODO: Basically all of the extra functionality in this
-func InitUser(userID byte) {
+import (
+	"encoding/binary"
+)
 
+//This initializes a user upon log-in. It returns the userID given, for later usage.
+//TODO: Basically all of the extra functionality in this
+func InitUser(userID []byte) uint16 {
+	return binary.BigEndian.Uint16(userID) //Thanks to https://stackoverflow.com/questions/11184336/how-to-convert-from-byte-to-int-in-go-programming
 }
 
 //This function is called if the SQL database cannot find the userID provided in InitUser.
