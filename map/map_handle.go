2cf66d83a848e680752f513c6d9991aa9d5c3e23
Connor Simmonds <connor.t.simmonds@gmail.com>
2020-02-24 15:49:39 +1300

    Made OpenMap create the filename, and createMap now simply takes the filename string (as it doesn't need to rebuild the info).

--- map/map_handle.go
+++ map/map_handle.go
@@ -16,25 +16,25 @@ func UpdateMap(x int, y int, value int, currentMap *os.File) {
 
 //Opens a map, and returns it for later use. If the file doesn't exist, it will create it.
 func OpenMap(userID uint16, dungeonID uint16, mapNum uint16) *os.File {
+	var stringBuilder strings.Builder
+	fileFormat := ".dng"
+	stringBuilder.WriteString("Maps/")
+	//Now we add our ints into our builder
+	stringBuilder.WriteString(strconv.FormatUint(uint64(userID), 10) + "/" + strconv.FormatUint(uint64(dungeonID), 10) + "_" + strconv.FormatUint(uint64(mapNum), 10) + fileFormat)
+	fmt.Print(stringBuilder.String())
 	file, fileError := os.Open("Maps/1/map1_1.dng")
 	if fileError != nil {
 		//There's been some kind of error, record it in the appropiate debug log (with a timestamp) and then create the map (failsafe)
 		fmt.Println(fileError.Error())
-		return createMap(dungeonID, mapNum, userID)
+		return createMap(stringBuilder.String())
 	} else {
 		return file
 	}
 }
 
 //Creates the map file
-func createMap(dungeonID uint16, mapNum uint16, userID uint16) *os.File {
-	var stringBuilder strings.Builder
+func createMap(filename string) *os.File {
 	var returnFile *os.File
-	fileFormat := ".dng"
-	stringBuilder.WriteString("Maps/")
-	//Now we add our ints into our builder
-	stringBuilder.WriteString(strconv.FormatUint(uint64(userID), 10) + "/" + strconv.FormatUint(uint64(dungeonID), 10) + "_" + strconv.FormatUint(uint64(mapNum), 10) + fileFormat)
-	fmt.Print(stringBuilder.String())
-	returnFile, _ = os.Create(stringBuilder.String())
+	returnFile, _ = os.Create(filename)
 	return returnFile //return the file
 }
